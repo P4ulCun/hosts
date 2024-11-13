c493e906e07f55b964ba0b415e3365e744c7d2f0
P4ulCun <yopaulc100@gmail.com>
2024-11-13 11:40:25 +0000

    Al doilea commit

--- ip_test.sh
+++ ip_test.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
+nume=$1
+ip=$2
+server=$3
 
-cat /etc/hosts | while read line; do
-
-  ip=$(echo $line | awk '{print $1}')
-  hostname=$(echo $line | awk '{print $2}')
-
-  nslookup_ip=$(nslookup $hostname | grep 'Adress: ' | tail -n 1 | awk '{print $2}')
-
-  if [[ "$ip" != "$nslookup_ip" ]];then
-    echo "Bogus IP for $hostname in /etc/hosts !"
-  fi
-done
+nslookup_ip=$(nslookup $nume $server | grep 'Adress: ' | tail -n 1 |awk '{print $2}')
+echo "$nslookup_ip"
+if [ "$ip" != "$nslookup_ip" ];then
+  echo "Bogus IP for $nume in /etc/hosts !"
+else
+  echo "IP-urile coincid, bravo!"
+fi
